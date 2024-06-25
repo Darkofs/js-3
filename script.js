b9e239d5e7bbb0101a0aefe1412f87e3a7dd8190
Sidharth S <sidharthshibu780@gmail.com>
2024-06-25 21:39:15 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const header = document.getElementById ("header")
+const button = document.getElementById ("changeTextButton")
+const text = document.querySelectorAll (".text")
+const list = document.querySelectorAll (".list-item")
+const firstListItem = document.querySelector (".list-item")
+console.log(firstListItem)
+button.addEventListener ("click",
+    function() {header.innerHTML="Text Changed!"}
+)
+for (let i = 0; i < text.length; i++) {
+    text[i].style.color="blue"
+}
+for (let i=0; i < list.length; i++) {
+    console.log(list[i].innerHTML);
+    list[i].addEventListener("click"
+       , function() {list[i].style.color="red"}
+    )
+}
+firstListItem.style.backgroundColor="yellow"
